c9f826043bc53de3d4d031a4fe9c04270b032ead
Jxnflzc <245186727@qq.com>
2021-09-08 19:47:16 +0800

    commit
    commit

---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,13 @@
+// nav
+module.exports = [
+  { text: '首页', link: '/' },
+  {
+    text: '索引',
+    link: '/archives/',
+    items: [
+      { text: '分类', link: '/categories/' },
+      { text: '标签', link: '/tags/' },
+      { text: '归档', link: '/archives/' },
+    ],
+  },
+]
